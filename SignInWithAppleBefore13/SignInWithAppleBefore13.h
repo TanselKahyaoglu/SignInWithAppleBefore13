e9df5a94cf7ad381a27016b264f7b6e8da753738
Tansel Kahyaoglu <tansel.kahyaoglu@appcent.mobi>
2020-09-04 12:11:29 +0300

    Initial Commit

---
+++ SignInWithAppleBefore13/SignInWithAppleBefore13.h
@@ -0,0 +1,19 @@
+//
+//  SignInWithAppleBefore13.h
+//  SignInWithAppleBefore13
+//
+//  Created by Tansel Kahyaoglu on 4.09.2020.
+//  Copyright © 2020 Tansel Kahyaoglu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SignInWithAppleBefore13.
+FOUNDATION_EXPORT double SignInWithAppleBefore13VersionNumber;
+
+//! Project version string for SignInWithAppleBefore13.
+FOUNDATION_EXPORT const unsigned char SignInWithAppleBefore13VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SignInWithAppleBefore13/PublicHeader.h>
+
+
